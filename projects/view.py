f72e354fc1d42098dc4c6f8448d6e1147981a737
Mohammed Khallaf <mohammed.khallaf.1997@gmail.com>
2022-01-19 08:14:01 +0200

    update view logic

--- projects/view.py
+++ projects/view.py
@@ -1,14 +1,13 @@
+from projects.db_queries import get_all_projects
+
+
 def view_projects():
     projects = []
     print("Hello to view projects menu")
-    with open("database/projects.txt") as fileObj:
-        for line in fileObj:
-            projects.append(line.split(';'))
-
     print("The existing projects:")
+    projects = get_all_projects()
+
     counter = 0
     for project in projects:
         counter += 1
         print(f"{counter} - {project[2]} with target: {project[4]}")
-
-    return projects
